5fd977700e49ba40d168a1bfd6b96a12e6195c79
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2025-06-05 15:50:48 -0500

    docs: Creation on /templates

--- docs/index.py
+++ docs/index.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
